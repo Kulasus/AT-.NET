da5845c5f8e0bf0a353eacaca3e03c8dc15eb075
Lukáš Kondiolka <48946785+Kulasus@users.noreply.github.com>
2020-09-23 10:15:45 +0200

    Added cv01 solution

---
+++ cv01-MyStack/MyStack/MyStack/EmptyStackException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyStack
+{
+    class EmptyStackException : Exception
+    {
+        public EmptyStackException()
+        {
+        }
+
+        public EmptyStackException(string message) : base(message)
+        {
+        }
+
+        public EmptyStackException(string message, Exception inner) : base(message, inner)
+        {
+        }
+    }
+}
---
+++ cv01-MyStack/MyStack/MyStack/MyStack.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyStack
+{
+
+    public delegate void StackEventHandler();
+    public class MyStack<T> : IEnumerable<T>
+    {
+        private T[] data;
+        private int currentIndex = 0;
+        public T top {
+            get {
+                if (currentIndex == 0) {
+                    throw new EmptyStackException("Stack is empty!");
+                }
+                else
+                    return data[currentIndex - 1];
+            }
+        }
+
+        public event StackEventHandler OnPush;
+        public event StackEventHandler OnPop;
+
+
+        public MyStack(int dataSize){
+            data = new T[dataSize];
+            OnPush += () => Console.WriteLine("Value added.");
+            OnPop += () => Console.WriteLine("Value removed.");
+        }
+
+        public void Push(T value)
+        {
+            data[currentIndex] = value;
+            currentIndex++;
+            OnPush?.Invoke();
+        }
+
+        public T Pop() 
+        { 
+        
+            OnPop?.Invoke();
+            currentIndex--;
+            return data[currentIndex];
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for(int i = 0; i < currentIndex; i++)
+            {
+                yield return data[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            for (int i = 0; i < currentIndex; i++)
+            {
+                yield return data[i];
+            }
+        }
+    }
+}
