0417e28ac54776f6028319b376eecc6ce04cb45a
Lukáš Kondiolka <48946785+Kulasus@users.noreply.github.com>
2020-11-03 17:25:27 +0100

    Added cv06 solution

---
+++ cv06-ASP_MVC3/ASP_NET_MVC3/ASP_NET_MVC3/Models/DeleteItemForm.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASP_NET_MVC3.Models
+{
+    public class DeleteItemForm
+    {
+        public string Text { get; set; }
+    }
+}
---
+++ cv06-ASP_MVC3/ASP_NET_MVC3/ASP_NET_MVC3/Models/ToDoForm.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASP_NET_MVC3.Models
+{
+    public class ToDoForm
+    {
+        [Required(ErrorMessage = "Musíte zadat text úkolu.")]
+        [Display(Name = "Úkol")]
+        public string Text { get; set; }
+    }
+}
