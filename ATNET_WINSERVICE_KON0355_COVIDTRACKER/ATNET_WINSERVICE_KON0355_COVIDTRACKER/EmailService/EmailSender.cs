52dde50b5d9aeb13977803bcb4442acfda3f8a75
Lukáš Kondiolka <48946785+Kulasus@users.noreply.github.com>
2020-11-14 15:59:45 +0100

    Reimplemented email sender to be more readable, fixed bug with sender address not displaying

--- ATNET_WINSERVICE_KON0355_COVIDTRACKER/ATNET_WINSERVICE_KON0355_COVIDTRACKER/EmailService/EmailSender.cs
+++ ATNET_WINSERVICE_KON0355_COVIDTRACKER/ATNET_WINSERVICE_KON0355_COVIDTRACKER/EmailService/EmailSender.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Xml;
 using ATNET_WINSERVICE_KON0355_COVIDTRACKER.Helpers;
-using MailKit;
+using MailKit.Net.Smtp;
 using MimeKit;
 
 namespace ATNET_WINSERVICE_KON0355_COVIDTRACKER.EmailService
@@ -16,33 +16,35 @@ namespace ATNET_WINSERVICE_KON0355_COVIDTRACKER.EmailService
         {
             XmlDocument xml = new XmlDocument();
             xml.Load(Session.settingsPath);
-            var messageToSend = new MimeMessage()
-            {
-                Sender = new MailboxAddress(
-                    xml.SelectSingleNode("/info/email/sender/name").InnerText.ToString(),
-                    xml.SelectSingleNode("/info/email/sender/address").InnerText.ToString()
-                    ),
-                Subject = xml.SelectSingleNode("/info/email/sender/subject").InnerText.ToString()
-            };
-            messageToSend.Body = new TextPart(MimeKit.Text.TextFormat.Html)
-            {
-                Text = @"Hey, this is just a test message."
-            };
-            messageToSend.To.Add(new MailboxAddress(
-                    xml.SelectSingleNode("/info/email/receiver/name").InnerText.ToString(),
-                    xml.SelectSingleNode("/info/email/receiver/address").InnerText.ToString()));
-            using (var smtp = new MailKit.Net.Smtp.SmtpClient())
-            {
-                smtp.Connect(
+
+            MimeMessage message = new MimeMessage();
+
+            message.From.Add(new MailboxAddress(
+                xml.SelectSingleNode("/info/email/sender/name").InnerText.ToString(),
+                xml.SelectSingleNode("/info/email/sender/address").InnerText.ToString()));
+
+            message.Subject = xml.SelectSingleNode("/info/email/sender/subject").InnerText.ToString();
+
+            message.To.Add(new MailboxAddress(
+                xml.SelectSingleNode("/info/email/receiver/name").InnerText.ToString(),
+                xml.SelectSingleNode("/info/email/receiver/address").InnerText.ToString()
+                ));
+
+            BodyBuilder bodyBuilder = new BodyBuilder();
+            bodyBuilder.HtmlBody = "<h1>Mamka je noob</h1>";
+            message.Body = bodyBuilder.ToMessageBody();
+
+            using SmtpClient client = new SmtpClient();
+            client.Connect(
                     xml.SelectSingleNode("/info/email/sender/smtp").InnerText.ToString(),
                     Int32.Parse(xml.SelectSingleNode("/info/email/sender/port").InnerText.ToString()),
                     true);
-                smtp.Authenticate(
+            client.Authenticate(
                     xml.SelectSingleNode("/info/email/sender/address").InnerText.ToString(),
-                    xml.SelectSingleNode("/info/email/sender/password").InnerText.ToString());
-                smtp.Send(messageToSend);
-                smtp.Disconnect(true);
-            }
+                    xml.SelectSingleNode("/info/email/sender/password").InnerText.ToString()
+                );
+            client.Send(message);
+            client.Disconnect(true);
         }
     }
 }
