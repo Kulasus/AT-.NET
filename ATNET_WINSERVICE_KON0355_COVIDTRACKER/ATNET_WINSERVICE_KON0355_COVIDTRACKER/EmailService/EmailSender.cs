e22671fbf8d45cad7a938abc60de5edc4d3cb87f
Lukáš Kondiolka <48946785+Kulasus@users.noreply.github.com>
2020-11-14 15:35:42 +0100

    Added basic implementation of email sender

---
+++ ATNET_WINSERVICE_KON0355_COVIDTRACKER/ATNET_WINSERVICE_KON0355_COVIDTRACKER/EmailService/EmailSender.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using ATNET_WINSERVICE_KON0355_COVIDTRACKER.Helpers;
+using MailKit;
+using MimeKit;
+
+namespace ATNET_WINSERVICE_KON0355_COVIDTRACKER.EmailService
+{
+    static class EmailSender
+    {
+        public static void sendMail()
+        {
+            XmlDocument xml = new XmlDocument();
+            xml.Load(Session.settingsPath);
+            var messageToSend = new MimeMessage()
+            {
+                Sender = new MailboxAddress(
+                    xml.SelectSingleNode("/info/email/sender/name").InnerText.ToString(),
+                    xml.SelectSingleNode("/info/email/sender/address").InnerText.ToString()
+                    ),
+                Subject = xml.SelectSingleNode("/info/email/sender/subject").InnerText.ToString()
+            };
+            messageToSend.Body = new TextPart(MimeKit.Text.TextFormat.Html)
+            {
+                Text = @"Hey, this is just a test message."
+            };
+            messageToSend.To.Add(new MailboxAddress(
+                    xml.SelectSingleNode("/info/email/receiver/name").InnerText.ToString(),
+                    xml.SelectSingleNode("/info/email/receiver/address").InnerText.ToString()));
+            using (var smtp = new MailKit.Net.Smtp.SmtpClient())
+            {
+                smtp.Connect(
+                    xml.SelectSingleNode("/info/email/sender/smtp").InnerText.ToString(),
+                    Int32.Parse(xml.SelectSingleNode("/info/email/sender/port").InnerText.ToString()),
+                    true);
+                smtp.Authenticate(
+                    xml.SelectSingleNode("/info/email/sender/address").InnerText.ToString(),
+                    xml.SelectSingleNode("/info/email/sender/password").InnerText.ToString());
+                smtp.Send(messageToSend);
+                smtp.Disconnect(true);
+            }
+        }
+    }
+}
