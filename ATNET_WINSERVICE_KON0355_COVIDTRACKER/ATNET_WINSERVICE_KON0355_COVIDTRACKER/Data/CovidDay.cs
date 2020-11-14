78ef46c9ceb704530a9dfc5c0ded88dfbc47c4a0
Lukáš Kondiolka <48946785+Kulasus@users.noreply.github.com>
2020-11-14 14:44:00 +0100

    Implemented Json deserialization

---
+++ ATNET_WINSERVICE_KON0355_COVIDTRACKER/ATNET_WINSERVICE_KON0355_COVIDTRACKER/Data/CovidDay.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace ATNET_WINSERVICE_KON0355_COVIDTRACKER.Data
+{
+    /// <summary>
+    /// Represents covid cases of one day
+    /// </summary>
+    class CovidDay
+    {
+        [JsonProperty("datum")]
+        private DateTime date { get; set; }
+
+        /// <summary>
+        /// Represents new covid cases
+        /// </summary>
+        [JsonProperty("prirustkovy_pocet_nakazenych")]
+        private long newCases { get; set; }
+
+        /// <summary>
+        /// Represents total covid cases to this day
+        /// </summary>
+        [JsonProperty("kumulativni_pocet_nakazenych")]
+        public long totalCases { get; set; }
+
+
+        public CovidDay(DateTime date, long newCases, long totalCases)
+        {
+            this.date = date;
+            this.newCases = newCases;
+            this.totalCases = totalCases;
+        }
+
+    }
+
+}
---
+++ ATNET_WINSERVICE_KON0355_COVIDTRACKER/ATNET_WINSERVICE_KON0355_COVIDTRACKER/Helpers/Session.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATNET_WINSERVICE_KON0355_COVIDTRACKER.Helpers
+{
+    /// <summary>
+    /// Helper class to utilize settingsPath location use
+    /// </summary>
+    static class Session
+    {
+        public static readonly string settingsPath = @"D:\Coding\AT-.NET-GIT\ATNET_WINSERVICE_KON0355_COVIDTRACKER\ATNET_WINSERVICE_KON0355_COVIDTRACKER\Resources\settings.xml";
+    }
+}
