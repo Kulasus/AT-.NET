21d879c49806831cf1629dd91b25629641ccaed7
Lukáš Kondiolka <48946785+Kulasus@users.noreply.github.com>
2020-12-01 19:09:59 +0100

    Added project setup and some basic design and implementation

---
+++ ATNET_WEBAPP_KON0355/ATNET_WEBAPP_KON0355/ATNET_WEBAPP_KON0355/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ATNET_WEBAPP_KON0355.Models;
+
+namespace ATNET_WEBAPP_KON0355.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public HomeController()
+        {
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(LoginForm form)
+        {
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction("Data", "Home");
+            }
+            return this.Login();
+
+        }
+
+        public IActionResult Data()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ ATNET_WEBAPP_KON0355/ATNET_WEBAPP_KON0355/ATNET_WEBAPP_KON0355/Models/LoginForm.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ATNET_WEBAPP_KON0355.Models
+{
+    public class LoginForm
+    {
+        [Required(ErrorMessage = "Username is required.")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "Password is required.")]
+        public string Password { get; set; }
+    }
+}
