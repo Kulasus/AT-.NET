f596232bc3fb1764f1a3e23263229e1051a7dc72
Lukáš Kondiolka <48946785+Kulasus@users.noreply.github.com>
2020-12-26 16:48:08 +0100

    Implemented grid view updating, deleting and basics of adding

---
+++ ATNET_WEBAPP_KON0355/ATNET_WEBAPP_KON0355/ATNET_WEBAPP_KON0355/Exceptions/ShowNotFoundException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ATNET_WEBAPP_KON0355.Exceptions
+{
+    public class ShowNotFoundException : Exception
+    {
+        public ShowNotFoundException()
+        {
+
+        }
+
+        public ShowNotFoundException(string message) : base(message)
+        {
+
+        }
+
+        public ShowNotFoundException(string message, Exception inner) : base(message, inner)
+        {
+
+        }
+    }
+}
---
+++ ATNET_WEBAPP_KON0355/ATNET_WEBAPP_KON0355/ATNET_WEBAPP_KON0355/Models/ShowForm.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ATNET_WEBAPP_KON0355.Models
+{
+    public class ShowForm
+    {
+        public int ID { get; set; }
+
+        public string Name { get; set; }
+
+        public int Series { get; set; }
+
+        public int Episodes { get; set; }
+
+        public string Character { get; set; }
+    }
+}
