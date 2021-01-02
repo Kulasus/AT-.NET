9a9e9a41d4f650b89635d728961bcdafbf51827e
Lukáš Kondiolka <48946785+Kulasus@users.noreply.github.com>
2021-01-02 18:10:44 +0100

    Moved ApiClient logic to plugin

--- ATNET_DESKTOP_KON0355_STOCKMETALPRICES/ATNET_DESKTOP_KON0355_STOCKMETALPRICES/GoldPlugin/GoldProcessor.cs
+++ ATNET_DESKTOP_KON0355_STOCKMETALPRICES/ATNET_DESKTOP_KON0355_STOCKMETALPRICES/GoldPlugin/GoldProcessor.cs
@@ -1,13 +1,80 @@
-﻿using System.Diagnostics;
-using SharedContext;
+﻿using SharedContext;
+using Newtonsoft.Json.Linq;
+using RestSharp;
+using System.Collections.Generic;
+using GoldPlugin.Properties;
+using System;
+using GoldPlugin.Exceptions;
 
 namespace GoldPlugin
 {
     public class GoldProcessor : IPlugin
     {
-        public void Greet()
+        private RestClient restClient { get; }
+        private RestRequest restRequest;
+        private static RestClient restClientStatus = new RestClient(Resources.ApiStatusUrl);
+        private static List<string> supportedCurrencies = new List<string>(new string[]
         {
-            Debug.WriteLine("Hello from GoldProcessor!");
+            Resources.EurMarking, Resources.CzkMarking, Resources.UsdMarking, Resources.GbpMarking
+        });
+        private static List<string> supportedMetals = new List<string>(new string[]
+        {
+            Resources.GoldMarking, Resources.SilverMarking, Resources.PalladiumMarking, Resources.PlatinumMarking
+        });
+        public GoldProcessor(string metal = null, string currency = null)
+        {
+            if (!CheckMetal(metal.ToUpper()) || metal == null)
+            {
+                metal = Resources.GoldMarking;
+            }
+            if (!CheckCurrency(currency.ToUpper()) || currency == null)
+            {
+                currency = Resources.CzkMarking;
+            }
+            restClient = new RestClient(Resources.ApiUrl + metal + "/" + currency);
+            restRequest = new RestRequest(Method.GET);
+            restRequest.AddHeader("x-access-token", Resources.X_access_token);
+            restRequest.AddHeader("Content-Type", Resources.Content_type);
         }
+
+        private bool CheckMetal(string metal)
+        {
+            return supportedMetals.Contains(metal);
+        }
+        private bool CheckCurrency(string currency)
+        {
+            return supportedCurrencies.Contains(currency);
+        }
+        public string GetJsonString()
+        {
+            IRestResponse response = restClient.Execute(restRequest);
+            if (response.IsSuccessful)
+            {
+                return response.Content;
+            }
+            else if (CheckApiStatus())
+            {
+                throw new ApiClientException(Resources.ApiClientInvalidRequestErrorMessage);
+            }
+            else
+            {
+                throw new ApiClientException(Resources.ApiClientResponseErrorMessage);
+            }
+        }
+        private bool CheckApiStatus()
+        {
+            IRestResponse response = restClientStatus.Execute(restRequest);
+            if (response.IsSuccessful)
+            {
+                var serviceStatusData = JObject.Parse(response.Content);
+                var status = Convert.ToBoolean(serviceStatusData["result"]);
+                return status;
+            }
+            else
+            {
+                throw new ApiClientException(Resources.ApiClientResponseErrorMessage);
+            }
+        }
+
     }
 }
--- ATNET_DESKTOP_KON0355_STOCKMETALPRICES/ATNET_DESKTOP_KON0355_STOCKMETALPRICES/SharedContext/IPlugin.cs
+++ ATNET_DESKTOP_KON0355_STOCKMETALPRICES/ATNET_DESKTOP_KON0355_STOCKMETALPRICES/SharedContext/IPlugin.cs
@@ -2,6 +2,6 @@
 {
     public interface IPlugin
     {
-        void Greet();
+        string GetJsonString();
     }
 }
